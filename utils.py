5bcc184591fcac81cb30d82a0b186f8756557e68
omphcompany <126784041+omphcompany@users.noreply.github.com>
2024-02-06 23:21:45 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,26 @@
-from google.generativeai import SafetySettings
-
-SAFETY_SETTINGS = SafetySettings()
+SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_SEXUAL",
+        "threshold": "ALLOW",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS",
+        "threshold": "ALLOW",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "ALLOW",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "ALLOW",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "ALLOW",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "ALLOW",
+    },
+]
